b6bddca1e46ab3aff1d57ce3a9bd8b3a7c3b0089
massiliatazit <massiliatazit@users.noreply.github.com>
2020-10-01 16:54:34 -0700

    Exo6.js

--- Exo6.js
+++ Exo6.js
@@ -3,3 +3,15 @@
 Write a function "check3and7" which accepts a positive number and check if it is a multiple of 3 or a multiple of 7.
 HINT: Module Operator
 */
+
+const check3and7 = function (x){
+
+ if ((x > 0 ) && ( x % 3 === 0) || (x % 7 === 0)) {
+
+    return true;
+
+ }
+
+}
+
+console.log(check3and7(12));
