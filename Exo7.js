04c52e0f8c0fdb7828c3cc3b9eff8f41b5c30f58
massiliatazit <massiliatazit@users.noreply.github.com>
2020-10-01 16:54:46 -0700

    Exo7.js

---
+++ Exo7.js
@@ -0,0 +1,13 @@
+/* EXERCISE 7
+Write a function "reverseString" to reverse programmatically a given string (es.: Strive => evirtS).
+*/
+
+const Reversestring = function(S){
+
+
+ return S .split('').reverse().join('') ;
+
+
+}
+
+console.log(Reversestring('strive'));
